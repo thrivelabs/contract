6647c84d383e660286cef79244a989846982a06c
THRIVE <33251238+thrivelabs@users.noreply.github.com>
2018-03-28 18:33:54 +0200

    Update ThriveToken.sol

--- ThriveToken.sol
+++ ThriveToken.sol
@@ -1,30 +1,18 @@
-pragma solidity ^0.4.18;
-
+pragma solidity ^0.4.21;
 
 import "./StandardToken.sol";
 
-
-/**
- * @title ThriveToken
- * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
- * Note they can later distribute these tokens as they wish using `transfer` and other
- * `StandardToken` functions.
- */
 contract ThriveToken is StandardToken {
 
-    string public constant name = "ThriveToken"; // solium-disable-line uppercase
-    string public constant symbol = "THRT"; // solium-disable-line uppercase
-    uint8 public constant decimals = 18; // solium-disable-line uppercase
+    string public constant name = "ThriveToken";
+    string public constant symbol = "THRT";
+    uint8 public constant decimals = 18;
 
     uint256 public constant INITIAL_SUPPLY = 280000000 * (10 ** uint256(decimals));
-    uint256 public constant RAISED_AMOUNT = 25000000;//Constant added for retrieving info about raised amount in ICO
 
-    /**
-     * @dev Constructor that gives msg.sender all of existing tokens.
-     */
     function ThriveToken() public {
         totalSupply_ = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
-        Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+        emit Transfer(0x0, msg.sender, INITIAL_SUPPLY);
     }
 }
